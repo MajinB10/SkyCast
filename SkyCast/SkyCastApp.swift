9a765b2b2407601c37339a71d8e5bb7113528c9b
Bhavesh <bhaveshultron@gmail.com>
2024-08-28 10:40:10 +0800

    Initial Commit

---
+++ SkyCast/SkyCastApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkyCastApp.swift
+//  SkyCast
+//
+//  Created by Bhavesh Anand on 28/8/24.
+//
+
+import SwiftUI
+
+@main
+struct SkyCastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
